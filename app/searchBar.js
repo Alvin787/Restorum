c8a3838ad80d83ea6649a7639cf261522f8b0409
Eddie Zhang <55262779+EddieZhang540@users.noreply.github.com>
2022-02-05 18:34:51 -0500

    Update searchBar.js

--- app/searchBar.js
+++ app/searchBar.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { View, TextInput, StyleSheet, Pressable, Text } from 'react-native';
 import { ContentCard } from '../src/components/ContentCard';;
 import { db } from '../src/firebase';
-import { collection, doc, query, where } from 'firebase/firestore';
+import { collection, doc, query, where, getDocs } from 'firebase/firestore';
 
 export default function SearchBar() {
   const [search, setSearch] = React.useState('');
   const [filters, setFilters] = React.useState({
-    'clothing': false,
+    'clothing': true,
     'technology/appliances': false,
     'furniture': false,
     'bathroom': false,
@@ -15,14 +15,20 @@ export default function SearchBar() {
     'kitchen': false,
   });
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     console.log(search);
     event.preventDefault();
     Object.entries(filters).map((tag) => {
-      console.log(tag[1].active);
-      if(tag[1].active){
-        const tagDocRef = db.collection('categories').doc(tag[0]).collection('posts');
+      console.log(tag[1]);
+      if(tag[1]){
+        const tagDocRef = collection(db, 'categories', tag[0], 'posts');
         // const q = query(tagDocRef, where())
+        getDocs(tagDocRef)
+        .then((response) => {
+          response.forEach((doc) => {
+            console.log(doc);
+          })
+        });
       }
     });
   }
